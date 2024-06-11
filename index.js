7872faeee2a8530f5ec69d06b315ff6a9dfd98f2
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-06-12 02:28:55 +0530

    component

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const bodyparser = require("body-parser");
-const nodemailer = require("nodemailer");
-const host = 5000;
+const bodyParser = require("body-parser");
 
 const app = express();
+const PORT = 5000;
+
 app.use(express.static("css"));
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Add this line to handle JSON data
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", (req, res) => {
-  const comm = req.body.email;
-  console.log(comm);
+app.post("/test", (req, res) => {
+  console.log("Test POST request received");
+  console.log("Request body:", req.body); // Debugging statement
+  res.send("Test route working");
 });
 
-app.listen(host, function () {
-  console.log("server running");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
